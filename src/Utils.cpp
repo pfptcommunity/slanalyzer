f4531835ca1c9e33c406355de321d93810a514bf
Ludvik Jerabek <ljerabek@proofpoint.com>
2022-11-17 14:32:22 -0500

    Add file coments

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,3 +1,11 @@
+/**
+ * This code was tested against C++20
+ *
+ * @author Ludvik Jerabek
+ * @package slanalyzer
+ * @version 1.0.0
+ * @license MIT
+ */
 #include "Utils.h"
 
 std::vector<std::string_view> Proofpoint::Utils::split(std::string_view str, char d)
