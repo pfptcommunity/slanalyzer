6c74316a1c1abc63fda3ee0009b6cc3a5b56a42c
Ludvik Jerabek <ljerabek@pfpt.io>
2026-05-11 11:16:14 -0400

    Fix includes for modern Linux
    Added inline conversion to std::str used for RE2 string views.

--- src/Utils.h
+++ src/Utils.h
@@ -13,16 +13,22 @@
 #include <vector>
 #include <string_view>
 
-namespace Proofpoint::Utils {
-void reverse(std::string& str);
-void ltrim(std::string& str);
-void rtrim(std::string& str);
-void trim(std::string& str);
-std::string reverse_copy(std::string s);
-std::string ltrim_copy(std::string s);
-std::string rtrim_copy(std::string s);
-std::string trim_copy(std::string s);
-std::vector<std::string_view> split(std::string_view str, char d);
+namespace Proofpoint::Utils
+{
+    void reverse(std::string& str);
+    void ltrim(std::string& str);
+    void rtrim(std::string& str);
+    void trim(std::string& str);
+    std::string reverse_copy(std::string s);
+    std::string ltrim_copy(std::string s);
+    std::string rtrim_copy(std::string s);
+    std::string trim_copy(std::string s);
+    std::vector<std::string_view> split(std::string_view str, char d);
+    template <typename T>
+    inline std::string cvt_std_string(const T& value)
+    {
+        return std::string(value.data(), value.size());
+    }
 }
 
 #endif //SLANALYZER_UTILS_H
