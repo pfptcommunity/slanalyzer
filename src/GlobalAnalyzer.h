419567d4744d60b2ed75303bd6939e54a77cec91
Ludvik Jerabek <ljerabek@pfpt.io>
2026-05-17 04:26:19 -0400

    Reformat

--- src/GlobalAnalyzer.h
+++ src/GlobalAnalyzer.h
@@ -13,22 +13,27 @@
 #include "GlobalAddressMatcher.h"
 #include "GlobalStringMatcher.h"
 
-namespace Proofpoint {
-class GlobalAnalyzer {
-public:
-	using PatternErrorMap = std::unordered_map<GlobalList::FieldType, PatternErrors<std::size_t>>;
-public:
-	GlobalAnalyzer() = default;
-	~GlobalAnalyzer() = default;
-	void Load(const GlobalList& safelist, PatternErrors<std::size_t>& pattern_errors);
-	std::optional<std::size_t> Process(const std::string& ss_file, GlobalList& safelist, std::size_t& records_processed);
-private:
-	GlobalAddressMatcher ip;
-	GlobalStringMatcher host;
-	GlobalStringMatcher helo;
-	GlobalStringMatcher hfrom;
-	GlobalStringMatcher from;
-	GlobalStringMatcher rcpt;
-};
+namespace Proofpoint
+{
+	class GlobalAnalyzer
+	{
+	public:
+		using PatternErrorMap = std::unordered_map<GlobalList::FieldType, PatternErrors<std::size_t>>;
+
+	public:
+		GlobalAnalyzer() = default;
+		~GlobalAnalyzer() = default;
+		void Load(const GlobalList& safelist, PatternErrors<std::size_t>& pattern_errors);
+		std::optional<std::size_t> Process(const std::string& ss_file, GlobalList& safelist,
+		                                   std::size_t& records_processed);
+
+	private:
+		GlobalAddressMatcher ip;
+		GlobalStringMatcher host;
+		GlobalStringMatcher helo;
+		GlobalStringMatcher hfrom;
+		GlobalStringMatcher from;
+		GlobalStringMatcher rcpt;
+	};
 }
 #endif //SLANALYZER_ANALYZER_H
