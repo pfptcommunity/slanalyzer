430c7cf05dcb5446ecfb0457e9e624ecce2adec8
Ludvik Jerabek <ljerabek@pfpt.io>
2026-05-17 04:28:59 -0400

    Reformat

--- src/IMatcher.h
+++ src/IMatcher.h
@@ -12,23 +12,29 @@
 #include <string>
 #include <vector>
 
-namespace Proofpoint {
-template <typename T>
-class IMatcher {
-public:
-	struct PatternError {
-	  T index;
-	  std::string pattern;
-	  std::string error;
+namespace Proofpoint
+{
+	template <typename T>
+	class IMatcher
+	{
+	public:
+		struct PatternError
+		{
+			T index;
+			std::string pattern;
+			std::string error;
+		};
+
+		using PatternErrors = std::vector<PatternError>;
+
+	public:
+		virtual void Add(const std::string& pattern, const T& index, PatternErrors& pattern_errors) = 0;
+		virtual bool Match(const std::string& pattern, std::vector<T>& match_indexes) = 0;
+		virtual std::size_t GetPatternCount() = 0;
 	};
-	using PatternErrors = std::vector<PatternError> ;
-public:
-	virtual void Add(const std::string& pattern, const T& index, PatternErrors& pattern_errors) = 0;
-	virtual bool Match(const std::string& pattern, std::vector<T>& match_indexes) = 0;
-	virtual std::size_t GetPatternCount() = 0;
-};
-template<typename T>
-using PatternErrors = typename IMatcher<T>::PatternErrors;
+
+	template <typename T>
+	using PatternErrors = typename IMatcher<T>::PatternErrors;
 }
 
 #endif //SLANALYZER_IMATCHER_H
