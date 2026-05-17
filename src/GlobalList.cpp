4c415eeecc4f80fe78ec7f2e6ccad5fca2841723
Ludvik Jerabek <ljerabek@pfpt.io>
2026-05-17 04:27:30 -0400

    Added  GetLineNumber for error logging

--- src/GlobalList.cpp
+++ src/GlobalList.cpp
@@ -15,39 +15,55 @@
 #include "re2/re2.h"
 #include <numeric>
 
-void Proofpoint::GlobalList::Load(const std::string& list_file, EntryErrors& entry_errors )
+void Proofpoint::GlobalList::Load(const std::string& list_file, EntryErrors& entry_errors)
 {
 	std::size_t line_number = 0;
 	std::ifstream f(list_file);
 	csv::CsvParser parser(f);
-	for (auto& row : parser) {
+	for (auto& row : parser)
+	{
 		line_number++;
 		size_t cols = row.size();
-		FieldType ft = (cols>0) ? GetFieldType(row.at(0)) : FieldType::UNKNOWN;
-		MatchType mt = (cols>1) ? GetMatchType(row.at(1)) : MatchType::UNKNOWN;
+		FieldType ft = (cols > 0) ? GetFieldType(row.at(0)) : FieldType::UNKNOWN;
+		MatchType mt = (cols > 1) ? GetMatchType(row.at(1)) : MatchType::UNKNOWN;
 
 		// Skip empty lines
-		if( cols == 1 && Utils::trim_copy(row.at(0)).empty() )
+		if (cols == 1 && Utils::trim_copy(row.at(0)).empty())
 			continue;
 
-		if (ft==FieldType::UNKNOWN || mt==MatchType::UNKNOWN) {
+		if (ft == FieldType::UNKNOWN || mt == MatchType::UNKNOWN)
+		{
 			// We are missing something.
-			entry_errors.push_back({line_number,
-					(cols>0) ? row.at(0) : "",
-					(cols>1) ? row.at(1) : "",
-					"Please see field type and match type information"});
+			entry_errors.push_back({
+				line_number,
+				GetFieldTypeString(ft),
+				GetMatchTypeString(mt),
+				"Please see field type and match type information"
+			});
+		}
+
+		// Not logic is highly dangerous and will be removed from Proofpoint Safe/Block lists in the future. We will notify.
+		if (mt == MatchType::IP_NOT_IN_NET || mt == MatchType::NOT_MATCH || mt == MatchType::NOT_REGEX || mt == MatchType::NOT_EQUAL)
+		{
+			entry_errors.push_back({
+				line_number,
+				GetFieldTypeString(ft),
+				GetMatchTypeString(mt),
+				"Negative match condition detected. Please see https://proofpoint.my.site.com/community/s/article/Email-Protection-PPS-PoD-Organizational-Safe-and-Block-List-Best-Practices."
+			});
 		}
 
 		entries.emplace_back();
 		entries.back().line_number = line_number;
 		entries.back().field_type = ft;
 		entries.back().match_type = mt;
-		if (cols>2) entries.back().pattern = row.at(2);
-		if (cols>3) entries.back().comment = row.at(3);
+		if (cols > 2) entries.back().pattern = row.at(2);
+		if (cols > 3) entries.back().comment = row.at(3);
 		entries.back().inbound = 0;
 		entries.back().outbound = 0;
 	}
 }
+
 void Proofpoint::GlobalList::Save(const std::string& list_file)
 {
 	std::size_t count = 0;
@@ -57,37 +73,39 @@ void Proofpoint::GlobalList::Save(const std::string& list_file)
 	std::ios_base::sync_with_stdio(false);
 	std::ofstream f(list_file);
 	f << "\"" << "FieldType"
-	  << "\",\"" << "MatchType"
-	  << "\",\"" << "Pattern"
-	  << "\",\"" << "Comment"
-	  << "\",\"" << "Inbound"
-	  << "\",\"" << "Outbound" << "\"\r\n";
-	for (auto& list_entry : entries) {
+		<< "\",\"" << "MatchType"
+		<< "\",\"" << "Pattern"
+		<< "\",\"" << "Comment"
+		<< "\",\"" << "Inbound"
+		<< "\",\"" << "Outbound" << "\"\r\n";
+	for (auto& list_entry : entries)
+	{
 		// Replaced for std::quoted() to improve speed
 		RE2::GlobalReplace(&list_entry.pattern, quoted, "\"\"");
 		RE2::GlobalReplace(&list_entry.comment, quoted, "\"\"");
 		f << "\"" << FieldTypeStrings[static_cast<int>(list_entry.field_type)]
-		  << "\",\"" << MatchTypeStrings[static_cast<int>(list_entry.match_type)]
-		  << "\",\"" << list_entry.pattern
-		  << "\",\"" << list_entry.comment
-		  << "\",\"" << list_entry.inbound
-		  << "\",\"" << list_entry.outbound << "\"\r\n";
+			<< "\",\"" << MatchTypeStrings[static_cast<int>(list_entry.match_type)]
+			<< "\",\"" << list_entry.pattern
+			<< "\",\"" << list_entry.comment
+			<< "\",\"" << list_entry.inbound
+			<< "\",\"" << list_entry.outbound << "\"\r\n";
 		count++;
 	}
 }
+
 inline Proofpoint::GlobalList::FieldType Proofpoint::GlobalList::GetFieldType(const std::string& field)
 {
-	if (strcmp("$ip", field.c_str())==0)
+	if (strcmp("$ip", field.c_str()) == 0)
 		return FieldType::IP;
-	if (strcmp("$host", field.c_str())==0)
+	if (strcmp("$host", field.c_str()) == 0)
 		return FieldType::HOST;
-	if (strcmp("$helo", field.c_str())==0)
+	if (strcmp("$helo", field.c_str()) == 0)
 		return FieldType::HELO;
-	if (strcmp("$rcpt", field.c_str())==0)
+	if (strcmp("$rcpt", field.c_str()) == 0)
 		return FieldType::RCPT;
-	if (strcmp("$from", field.c_str())==0)
+	if (strcmp("$from", field.c_str()) == 0)
 		return FieldType::FROM;
-	if (strcmp("$hfrom", field.c_str())==0)
+	if (strcmp("$hfrom", field.c_str()) == 0)
 		return FieldType::HFROM;
 	return FieldType::UNKNOWN;
 }
@@ -100,36 +118,45 @@ inline const std::string& Proofpoint::GlobalList::GetFieldTypeString(Proofpoint:
 inline Proofpoint::GlobalList::MatchType Proofpoint::GlobalList::GetMatchType(const std::string& field)
 {
 	// strcmp has consistently better times than string.compare or ==
-	if (strcmp("equal", field.c_str())==0)
+	if (strcmp("equal", field.c_str()) == 0)
 		return MatchType::EQUAL;
-	else if (strcmp("not_equal", field.c_str())==0)
+	else if (strcmp("not_equal", field.c_str()) == 0)
 		return MatchType::NOT_EQUAL;
-	else if (strcmp("match", field.c_str())==0)
+	else if (strcmp("match", field.c_str()) == 0)
 		return MatchType::MATCH;
-	else if (strcmp("not_match", field.c_str())==0)
+	else if (strcmp("not_match", field.c_str()) == 0)
 		return MatchType::NOT_MATCH;
-	else if (strcmp("regex", field.c_str())==0)
+	else if (strcmp("regex", field.c_str()) == 0)
 		return MatchType::REGEX;
-	else if (strcmp("not_regex", field.c_str())==0)
+	else if (strcmp("not_regex", field.c_str()) == 0)
 		return MatchType::NOT_REGEX;
-	else if (strcmp("ip_in_net", field.c_str())==0)
+	else if (strcmp("ip_in_net", field.c_str()) == 0)
 		return MatchType::IP_IN_NET;
-	else if (strcmp("ip_not_in_net", field.c_str())==0)
+	else if (strcmp("ip_not_in_net", field.c_str()) == 0)
 		return MatchType::IP_NOT_IN_NET;
-	else if (strcmp("is_in_domainset", field.c_str())==0)
+	else if (strcmp("is_in_domainset", field.c_str()) == 0)
 		return MatchType::IS_IN_DOMAINSET;
 	else
 		return MatchType::UNKNOWN;
 }
+
 inline const std::string& Proofpoint::GlobalList::GetMatchTypeString(Proofpoint::GlobalList::MatchType matchtype)
 {
 	return MatchTypeStrings[static_cast<int>(matchtype)];
 }
+
 std::size_t Proofpoint::GlobalList::GetInboundCount() const
 {
-	return std::accumulate(entries.begin(), entries.end(), 0, [](const std::size_t& a, const Entry& b) -> std::size_t {return a + b.inbound;} );
+	return std::accumulate(entries.begin(), entries.end(), 0, [](const std::size_t& a, const Entry& b) -> std::size_t
+	{
+		return a + b.inbound;
+	});
 }
+
 std::size_t Proofpoint::GlobalList::GetOutboundCount() const
 {
-	return std::accumulate(entries.begin(), entries.end(), 0, [](const std::size_t& a, const Entry& b) -> std::size_t {return a + b.outbound;} );
+	return std::accumulate(entries.begin(), entries.end(), 0, [](const std::size_t& a, const Entry& b) -> std::size_t
+	{
+		return a + b.outbound;
+	});
 }
--- src/GlobalList.h
+++ src/GlobalList.h
@@ -12,74 +12,98 @@
 #include <memory>
 #include <vector>
 
-namespace Proofpoint {
-class GlobalList {
-	friend class GlobalAnalyzer;
-public:
-	enum class FieldType {
-	  UNKNOWN,
-	  IP,
-	  HOST,
-	  HELO,
-	  RCPT,
-	  FROM,
-	  HFROM
+namespace Proofpoint
+{
+	class GlobalList
+	{
+		friend class GlobalAnalyzer;
+
+	public:
+		enum class FieldType
+		{
+			UNKNOWN,
+			IP,
+			HOST,
+			HELO,
+			RCPT,
+			FROM,
+			HFROM
+		};
+
+		enum class MatchType
+		{
+			UNKNOWN,
+			EQUAL,
+			NOT_EQUAL,
+			MATCH,
+			NOT_MATCH,
+			REGEX,
+			NOT_REGEX,
+			IP_IN_NET,
+			IP_NOT_IN_NET,
+			IS_IN_DOMAINSET
+		};
+
+		struct Entry
+		{
+			std::size_t line_number;
+			FieldType field_type;
+			MatchType match_type;
+			std::string pattern;
+			std::string comment;
+			uint32_t inbound;
+			uint32_t outbound;
+		};
+
+		using Entries = std::vector<Entry>;
+
+		struct EntryError
+		{
+			std::size_t line_number;
+			std::string field_data;
+			std::string match_data;
+			std::string error;
+		};
+
+		using EntryErrors = std::vector<EntryError>;
+		using iterator = Entries::iterator;
+		using const_iterator = Entries::const_iterator;
+
+	public:
+		static FieldType GetFieldType(const std::string& field);
+		static const std::string& GetFieldTypeString(FieldType field);
+		static MatchType GetMatchType(const std::string& field);
+		static const std::string& GetMatchTypeString(MatchType matchtype);
+
+	private:
+		inline static const std::string FieldTypeStrings[] = {
+			"unknown", "$ip", "$host", "$helo", "$rcpt", "$from", "$hfrom"
+		};
+		inline static const std::string MatchTypeStrings[] = {
+			"unknown", "equal", "not_equal", "match", "not_match", "regex", "not_regex", "ip_in_net", "ip_not_in_net",
+			"is_in_domainset"
+		};
+
+	public:
+		GlobalList() = default;
+		void Load(const std::string& list_file, EntryErrors& entry_errors);
+		void Save(const std::string& list_file);
+
+	public:
+		[[nodiscard]] inline std::size_t GetCount() const { return entries.size(); }
+		[[nodiscard]] std::size_t GetInboundCount() const;
+		[[nodiscard]] std::size_t GetOutboundCount() const;
+		iterator begin() { return entries.begin(); }
+		iterator end() { return entries.end(); }
+		[[nodiscard]] const_iterator begin() const { return entries.begin(); }
+		[[nodiscard]] const_iterator end() const { return entries.end(); }
+		[[nodiscard]] const_iterator cbegin() const { return entries.cbegin(); }
+		[[nodiscard]] const_iterator cend() const { return entries.cend(); }
+		[[nodiscard]] const Entry& operator[](std::size_t index) const { return entries.at(index); }
+		[[nodiscard]] std::size_t GetLineNumber(std::size_t index) const { return entries.at(index).line_number; }
+
+	private:
+		Entries entries;
 	};
-	enum class MatchType {
-	  UNKNOWN,
-	  EQUAL,
-	  NOT_EQUAL,
-	  MATCH,
-	  NOT_MATCH,
-	  REGEX,
-	  NOT_REGEX,
-	  IP_IN_NET,
-	  IP_NOT_IN_NET,
-	  IS_IN_DOMAINSET
-	};
-	struct Entry {
-	  std::size_t line_number;
-	  FieldType field_type;
-	  MatchType match_type;
-	  std::string pattern;
-	  std::string comment;
-	  uint32_t inbound;
-	  uint32_t outbound;
-	};
-	using Entries = std::vector<Entry>;
-	struct EntryError {
-	  std::size_t line;
-	  std::string field_data;
-	  std::string match_data;
-	  std::string error;
-	};
-	using EntryErrors = std::vector<EntryError>;
-	using iterator = Entries::iterator;
-	using const_iterator = Entries::const_iterator;
-public:
-	static FieldType GetFieldType(const std::string& field);
-	static const std::string& GetFieldTypeString(FieldType field);
-	static MatchType GetMatchType(const std::string& field);
-	static const std::string& GetMatchTypeString(MatchType matchtype);
-private:
-	inline static const std::string FieldTypeStrings[] = {"unknown", "$ip", "$host", "$helo", "$rcpt", "$from", "$hfrom"};
-	inline static const std::string MatchTypeStrings[] = {"unknown", "equal", "not_equal", "match", "not_match", "regex", "not_regex", "ip_in_net", "ip_not_in_net", "is_in_domainset"};
-public:
-	GlobalList() = default;
-	void Load(const std::string& list_file, EntryErrors& entry_errors);
-	void Save(const std::string& list_file);
-public:
-	[[nodiscard]] inline std::size_t GetCount() const { return entries.size(); }
-	[[nodiscard]] std::size_t GetInboundCount() const;
-	[[nodiscard]] std::size_t GetOutboundCount() const;
-	iterator begin() { return entries.begin(); }
-	iterator end() { return entries.end(); }
-	[[nodiscard]] const_iterator begin() const { return entries.begin(); }
-	[[nodiscard]] const_iterator end() const { return entries.end(); }
-	[[nodiscard]] const_iterator cbegin() const { return entries.cbegin(); }
-	[[nodiscard]] const_iterator cend() const { return entries.cend(); }
-private:
-	Entries entries;
-};
 }
 #endif //SLANALYZER_SAFELIST_H
