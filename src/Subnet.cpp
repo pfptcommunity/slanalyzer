7b94f71b3d11f718387dddf1dc9c7c8df7247a3d
Ludvik Jerabek <ljerabek@pfpt.io>
2026-05-17 04:31:17 -0400

    Reformat

--- src/Subnet.cpp
+++ src/Subnet.cpp
@@ -14,18 +14,18 @@
 #include "re2/re2.h"
 
 Proofpoint::Subnet::Subnet(const std::string& cidr)
-		:min(0), max(0), wmask(0), hosts(0)
+	: min(0), max(0), wmask(0), hosts(0)
 {
 	re2::StringPiece matches[3];
 
 	if (!cidr_matcher.Match(cidr, 0, cidr.length(), RE2::ANCHOR_BOTH, matches, 3))
-		throw SubnetArgumentException("Invalid CIDR format ["+cidr+"]");
+		throw SubnetArgumentException("Invalid CIDR format [" + cidr + "]");
 
 	in_addr net_address = {0};
 
 	std::string address = Utils::cvt_std_string(matches[1]);
 	if (inet_pton(AF_INET, address.c_str(), &net_address) != 1)
-		throw SubnetArgumentException("Invalid network address format ["+address+"]");
+		throw SubnetArgumentException("Invalid network address format [" + address + "]");
 
 	std::string bits = Utils::cvt_std_string(matches[2]);
 	unsigned long b = atoi(bits.c_str());
@@ -38,114 +38,136 @@ Proofpoint::Subnet::Subnet(const std::string& cidr)
 	wmask = ~mask;
 	bcast = net | wmask;
 
-	if (b >= 31) {
+	if (b >= 31)
+	{
 		min = net;
 		max = bcast;
 		hosts = 0;
 	}
-	else {
+	else
+	{
 		min = net + 1;
 		max = bcast - 1;
 		hosts = wmask - 1;
 	}
 }
+
 Proofpoint::Subnet::Subnet(const std::string& network, const std::string& netmask)
 {
 	in_addr net_address = {0};
 	in_addr mask_address = {0};
 
 	if (inet_pton(AF_INET, network.c_str(), &net_address) != 1)
-		throw SubnetArgumentException("Invalid network address format ["+network+"]");
+		throw SubnetArgumentException("Invalid network address format [" + network + "]");
 
 	if (inet_pton(AF_INET, netmask.c_str(), &mask_address) != 1)
-		throw SubnetArgumentException("Invalid mask address format ["+netmask+"]");
+		throw SubnetArgumentException("Invalid mask address format [" + netmask + "]");
 
 	this->net = ntohl(net_address.s_addr);
 	this->mask = ntohl(mask_address.s_addr);
 
-	if ((mask & (~mask >> 1))) {
-		throw SubnetArgumentException("Invalid mask address ["+GetAddress(mask)+"]");
+	if ((mask & (~mask >> 1)))
+	{
+		throw SubnetArgumentException("Invalid mask address [" + GetAddress(mask) + "]");
 	}
 	net = net & mask;
 	wmask = ~mask;
 	bcast = net | wmask;
-	min = net+1;
-	max = bcast-1;
-	hosts = wmask-1;
+	min = net + 1;
+	max = bcast - 1;
+	hosts = wmask - 1;
 }
+
 Proofpoint::Subnet::Subnet(const in_addr_t& network, const in_addr_t& netmask, Proofpoint::Subnet::ByteOrder order)
 {
-	if (order==ByteOrder::HOST) {
+	if (order == ByteOrder::HOST)
+	{
 		net = network;
 		mask = netmask;
 	}
-	else {
+	else
+	{
 		net = ntohl(network);
 		mask = ntohl(netmask);
 	}
 	net = net & mask;
 	wmask = ~mask;
 	bcast = net | wmask;
-	min = net+1;
-	max = bcast-1;
-	hosts = wmask-1;
+	min = net + 1;
+	max = bcast - 1;
+	hosts = wmask - 1;
 }
+
 std::string Proofpoint::Subnet::GetNet() const
 {
 	return GetAddress(this->net);
 }
+
 std::string Proofpoint::Subnet::GetMask() const
 {
 	return GetAddress(this->mask);
 }
+
 std::string Proofpoint::Subnet::GetMin() const
 {
 	return GetAddress(this->min);
 }
+
 std::string Proofpoint::Subnet::GetMax() const
 {
 	return GetAddress(this->max);
 }
+
 std::string Proofpoint::Subnet::GetBroadcast() const
 {
 	return GetAddress(this->bcast);
 }
+
 std::string Proofpoint::Subnet::GetWildcard() const
 {
 	return GetAddress(this->wmask);
 }
+
 in_addr_t Proofpoint::Subnet::GetNetAddress(Proofpoint::Subnet::ByteOrder order) const
 {
-	return (order==ByteOrder::HOST) ? net : htonl(net);
+	return (order == ByteOrder::HOST) ? net : htonl(net);
 }
+
 in_addr_t Proofpoint::Subnet::GetMaskAddress(Proofpoint::Subnet::ByteOrder order) const
 {
-	return (order==ByteOrder::HOST) ? mask : htonl(mask);
+	return (order == ByteOrder::HOST) ? mask : htonl(mask);
 }
+
 in_addr_t Proofpoint::Subnet::GetMinAddress(Proofpoint::Subnet::ByteOrder order) const
 {
-	return (order==ByteOrder::HOST) ? min : htonl(min);
+	return (order == ByteOrder::HOST) ? min : htonl(min);
 }
+
 in_addr_t Proofpoint::Subnet::GetMaxAddress(Proofpoint::Subnet::ByteOrder order) const
 {
-	return (order==ByteOrder::HOST) ? max : htonl(max);
+	return (order == ByteOrder::HOST) ? max : htonl(max);
 }
+
 in_addr_t Proofpoint::Subnet::GetBroadcastAddress(Proofpoint::Subnet::ByteOrder order) const
 {
-	return (order==ByteOrder::HOST) ? bcast : htonl(bcast);
+	return (order == ByteOrder::HOST) ? bcast : htonl(bcast);
 }
+
 in_addr_t Proofpoint::Subnet::GetWildcardAddress(Proofpoint::Subnet::ByteOrder order) const
 {
-	return (order==ByteOrder::HOST) ? wmask : htonl(wmask);
+	return (order == ByteOrder::HOST) ? wmask : htonl(wmask);
 }
+
 uint32_t Proofpoint::Subnet::GetAddressableHosts() const
 {
 	return hosts;
 }
+
 bool Proofpoint::Subnet::IsValidIp(const std::string& address)
 {
 	return RE2::FullMatch(address, ip_matcher);
 }
+
 bool Proofpoint::Subnet::IsValidCidr(const std::string& cidr, std::string& network, std::string& bits)
 {
 	re2::StringPiece matches[3];
@@ -154,14 +176,16 @@ bool Proofpoint::Subnet::IsValidCidr(const std::string& cidr, std::string& netwo
 	bits = Utils::cvt_std_string(matches[2]);
 	return true;
 }
+
 bool Proofpoint::Subnet::IsValidCidr(const std::string& cidr)
 {
 	return RE2::FullMatch(cidr, cidr_matcher);
 }
+
 std::string Proofpoint::Subnet::GetAddress(in_addr_t address, Proofpoint::Subnet::ByteOrder order)
 {
 	char s[INET_ADDRSTRLEN];
-	if (order==ByteOrder::HOST)
+	if (order == ByteOrder::HOST)
 		address = htonl(address);
 	inet_ntop(AF_INET, &address, s, INET_ADDRSTRLEN);
 	return s;
--- src/Subnet.h
+++ src/Subnet.h
@@ -13,69 +13,84 @@
 #include <stdexcept>
 #include "re2/re2.h"
 
-namespace Proofpoint {
-class Subnet {
-public:
-	class SubnetArgumentException : public std::invalid_argument {
-		using std::invalid_argument::invalid_argument;
-	};
-	enum ByteOrder {
-	  NETWORK,
-	  HOST,
-	};
-public:
-	static bool IsValidIp(const std::string& address);
-	static bool IsValidCidr(const std::string& cidr, std::string& network, std::string& bits);
-	static bool IsValidCidr(const std::string& cidr);
-	static std::string GetAddress(in_addr_t address, ByteOrder order = HOST);
-public:
-	explicit Subnet(const std::string& cidr);
-	explicit Subnet(const std::string& network, const std::string& netmask);
-	explicit Subnet(const in_addr_t& network, const in_addr_t& netmask, ByteOrder order);
-	inline bool InSubnet(in_addr_t address, Proofpoint::Subnet::ByteOrder order) const
+namespace Proofpoint
+{
+	class Subnet
 	{
-		if (order==ByteOrder::NETWORK) {
-			address = ntohl(address);
+	public:
+		class SubnetArgumentException : public std::invalid_argument
+		{
+			using std::invalid_argument::invalid_argument;
+		};
+
+		enum ByteOrder
+		{
+			NETWORK,
+			HOST,
+		};
+
+	public:
+		static bool IsValidIp(const std::string& address);
+		static bool IsValidCidr(const std::string& cidr, std::string& network, std::string& bits);
+		static bool IsValidCidr(const std::string& cidr);
+		static std::string GetAddress(in_addr_t address, ByteOrder order = HOST);
+
+	public:
+		explicit Subnet(const std::string& cidr);
+		explicit Subnet(const std::string& network, const std::string& netmask);
+		explicit Subnet(const in_addr_t& network, const in_addr_t& netmask, ByteOrder order);
+
+		inline bool InSubnet(in_addr_t address, Proofpoint::Subnet::ByteOrder order) const
+		{
+			if (order == ByteOrder::NETWORK)
+			{
+				address = ntohl(address);
+			}
+			return !((address ^ net) & mask);
 		}
-		return !((address ^ net) & mask);
-	}
-	inline bool InSubnet(const std::string& ip_address) const
-	{
-		in_addr address{0};
-		if (inet_pton(AF_INET, ip_address.c_str(), &address)==0) return false;
-		return !((ntohl(address.s_addr) ^ net) & mask);
-	}
-	[[nodiscard]] std::string GetNet() const;
-	[[nodiscard]] std::string GetMask() const;
-	[[nodiscard]] std::string GetMin() const;
-	[[nodiscard]] std::string GetMax() const;
-	[[nodiscard]] std::string GetBroadcast() const;
-	[[nodiscard]] std::string GetWildcard() const;
-	[[nodiscard]] in_addr_t GetNetAddress(ByteOrder order = HOST) const;
-	[[nodiscard]] in_addr_t GetMaskAddress(ByteOrder order = HOST) const;
-	[[nodiscard]] in_addr_t GetMinAddress(ByteOrder order = HOST) const;
-	[[nodiscard]] in_addr_t GetMaxAddress(ByteOrder order = HOST) const;
-	[[nodiscard]] in_addr_t GetBroadcastAddress(ByteOrder order = HOST) const;
-	[[nodiscard]] in_addr_t GetWildcardAddress(ByteOrder order = HOST) const;
-	[[nodiscard]] uint32_t GetAddressableHosts() const;
-private:
-	inline static RE2 ip_matcher = R"(^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$)";
-	inline static RE2 cidr_matcher = R"(^((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))(?:\/)((?:3[0-2]|2[0-9]|1[0-9]|[0-9]))$)";
-private:
-	// Network Address (host order)
-	in_addr_t net;
-	// Subnet Mask (host order)
-	in_addr_t mask;
-	// Lowest Address (host order)
-	in_addr_t min{};
-	// Highest Address
-	in_addr_t max{};
-	// Broadcast
-	in_addr_t bcast{};
-	// Wildcard
-	in_addr_t wmask{};
-	// Maximum addressable
-	uint32_t hosts{};
-};
+
+		inline bool InSubnet(const std::string& ip_address) const
+		{
+			in_addr address{0};
+			if (inet_pton(AF_INET, ip_address.c_str(), &address) == 0) return false;
+			return !((ntohl(address.s_addr) ^ net) & mask);
+		}
+
+		[[nodiscard]] std::string GetNet() const;
+		[[nodiscard]] std::string GetMask() const;
+		[[nodiscard]] std::string GetMin() const;
+		[[nodiscard]] std::string GetMax() const;
+		[[nodiscard]] std::string GetBroadcast() const;
+		[[nodiscard]] std::string GetWildcard() const;
+		[[nodiscard]] in_addr_t GetNetAddress(ByteOrder order = HOST) const;
+		[[nodiscard]] in_addr_t GetMaskAddress(ByteOrder order = HOST) const;
+		[[nodiscard]] in_addr_t GetMinAddress(ByteOrder order = HOST) const;
+		[[nodiscard]] in_addr_t GetMaxAddress(ByteOrder order = HOST) const;
+		[[nodiscard]] in_addr_t GetBroadcastAddress(ByteOrder order = HOST) const;
+		[[nodiscard]] in_addr_t GetWildcardAddress(ByteOrder order = HOST) const;
+		[[nodiscard]] uint32_t GetAddressableHosts() const;
+
+	private:
+		inline static RE2 ip_matcher =
+			R"(^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$)";
+		inline static RE2 cidr_matcher =
+			R"(^((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))(?:\/)((?:3[0-2]|2[0-9]|1[0-9]|[0-9]))$)";
+
+	private:
+		// Network Address (host order)
+		in_addr_t net;
+		// Subnet Mask (host order)
+		in_addr_t mask;
+		// Lowest Address (host order)
+		in_addr_t min{};
+		// Highest Address
+		in_addr_t max{};
+		// Broadcast
+		in_addr_t bcast{};
+		// Wildcard
+		in_addr_t wmask{};
+		// Maximum addressable
+		uint32_t hosts{};
+	};
 }
 #endif //SLANALYZER_SUBNET_H
